fe9c18f4d93b4eb7443117cd5ccc6e6bc3db7d8d
Arsene <komezarsene15@gmail.com>
2021-12-18 03:39:50 +0530

    react app deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
-import Menu from './Components/Menu';
-import { DISHES } from './Shared/Dishes';
+import Main from './Components/Maincomponent';
 
-class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
+
+class App extends Component{ 
+
   render(){
     return(
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes}/>
+       
+        <Main />
 
       </div>
     );
