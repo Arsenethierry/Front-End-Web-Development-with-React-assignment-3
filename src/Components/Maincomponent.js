b5cbe7ca0b030effaf730a63881b3c00779d7b68
Arsene <komezarsene15@gmail.com>
2022-01-02 00:44:33 +0530

    Single Page Applications Part 1

--- src/Components/Maincomponent.js
+++ src/Components/Maincomponent.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 import Home from "./HomeComponent";
 import Menu from './Menu';
+import Contact from './ContactComponent';
 import DishDetail from './Dishdetail';
 import Header from "./HeaderComponent";
 import Footer from './FooterComponent';
 import { DISHES } from '../Shared/Dishes';
+import { COMMENTS } from '../Shared/comments';
+import { PROMOTIONS } from '../Shared/promotions';
+import { LEADERS } from '../Shared/leaders';
 import { Route, Redirect, Switch } from 'react-router-dom';
+
 
 class Main extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
   
@@ -22,15 +30,28 @@ class Main extends Component{
   render(){
     const HomePage=()=>{
       return(
-        <Home />
+        <Home
+        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+         />
       );
     }
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     return( 
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes} />} />
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
