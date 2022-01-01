c494e437d13661b61f9fb2ad2ca560c2777d65a1
Arsene <komezarsene15@gmail.com>
2022-01-02 05:18:28 +0530

    Single Page Applications Part 2

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -5,11 +5,12 @@ import { Link } from 'react-router-dom';
 function RenderMenuItem({ dish, onClick}){
     return(
      <Card>
-         <Link to={`menu/${dish.id}`}/>
+         <Link to={`menu/${dish.id}`}>
         <CardImg width="100%" src={ dish.image } alt={ dish.name } />
         <CardImgOverlay>
             <CardTitle> { dish.name }</CardTitle>
         </CardImgOverlay>
+        </Link>
       </Card> 
     );
 }
